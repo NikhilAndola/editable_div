f29454fe0c0c282e83d217e3fd2e06fd8d27f4e2
nikhilAndola <nikhilandola123@gmail.com>
2021-08-26 23:18:48 +0530

    Finished project

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 console.log("How to make a editable div");
 
-// creating element and and text node. 
+// creating a new element 
 let divElem = document.createElement("div");
-let text = document.createTextNode("This is my element. Click to edit it.");
+
+// creating a text node and checking it with local Storage.
+let val = localStorage.getItem("text");
+    let text;
+    if (val == null){
+        text = document.createTextNode("This is my element. Click to edit it.");
+    } else {
+        text = document.createTextNode(val);
+    }
 
 // setting attributes for created div (divElem)
 divElem.setAttribute("id","elem");
 divElem.setAttribute("class","elem");
-divElem.setAttribute("style","border:2px solid red; width: 154px; margin:34px; padding: 23px;");
+divElem.setAttribute("style","border:2px solid red; width: 254px; margin:34px; padding: 23px;");
 
 // inserting the text into divElem using appendChild.
 divElem.appendChild(text);
@@ -20,7 +28,21 @@ let first = document.getElementById("myFirst");
 container.insertBefore(divElem, first); 
 console.log(divElem, container, first);
 
-Adding
+// Adding event listener to divElem.
 
+divElem.addEventListener("click", () => {
+    let noTextArea = document.getElementsByClassName("textarea").length;
+    if(noTextArea == 0){
+        let html = elem.innerHTML;
+        divElem.innerHTML = `<textarea id="textarea" class="textarea" rows="3">${html}</textarea>`
+    }
+    // Listen for the blur event on textarea
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener("blur", () => {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+    
+})
 
 
